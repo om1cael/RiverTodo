1a280e580207e588aa347b7f4aa2cd48d38af1cc
om1cael <188113439+om1cael@users.noreply.github.com>
2026-01-12 18:35:57 -0300

    feat(todo_item): add id field

--- lib/domain/entities/todo_item.dart
+++ lib/domain/entities/todo_item.dart
@@ -1,8 +1,10 @@
 class TodoItem {
+  final int id;
   final String description;
   final bool isDone;
 
   const TodoItem({
+    required this.id,
     required this.description,
     this.isDone = false,
   });
