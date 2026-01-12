41fc57f1e945298864c2268efc146cef5f688b53
om1cael <188113439+om1cael@users.noreply.github.com>
2026-01-12 20:03:18 -0300

    feat(home_view_model): create notifier provider

--- lib/ui/home/view_model/home_view_model.dart
+++ lib/ui/home/view_model/home_view_model.dart
@@ -2,6 +2,8 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:rivertodo/data/repositories/todo_repository_impl.dart';
 import 'package:rivertodo/domain/entities/todo_item.dart';
 
+final homeViewModelProvider = NotifierProvider(HomeViewModel.new);
+
 class HomeViewModel extends Notifier<List<TodoItem>> {
   @override
   List<TodoItem> build() {
