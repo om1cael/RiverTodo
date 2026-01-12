e1c5b6601b3d2ca28923a69a393b63e7aa4e3991
om1cael <188113439+om1cael@users.noreply.github.com>
2026-01-12 19:08:26 -0300

    feat(todo_item): make id required

--- lib/domain/entities/todo_item.dart
+++ lib/domain/entities/todo_item.dart
@@ -1,9 +1,10 @@
 class TodoItem {
-  int? id;
+  final int id;
   final String description;
   final bool isDone;
 
   TodoItem({
+    required this.id,
     required this.description,
     this.isDone = false,
   });
