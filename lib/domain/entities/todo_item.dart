702f5d0b0b1cb6a4e8a118d1b521e3a6b536fa4b
om1cael <188113439+om1cael@users.noreply.github.com>
2026-01-12 18:44:52 -0300

    feat(todo_item): make id nullable

--- lib/domain/entities/todo_item.dart
+++ lib/domain/entities/todo_item.dart
@@ -1,10 +1,9 @@
 class TodoItem {
-  final int id;
+  int? id;
   final String description;
   final bool isDone;
 
-  const TodoItem({
-    required this.id,
+  TodoItem({
     required this.description,
     this.isDone = false,
   });
